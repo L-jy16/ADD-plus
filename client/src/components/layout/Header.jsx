212b9f16b27176b620623bdcdd954469b2d4bcd4
L-jy16 <144635774+L-jy16@users.noreply.github.com>
2023-12-20 01:32:30 +0900

    2023.12.20

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/img/header_logo.png';
 
 const Header = () => {
+    const [linkActive, setLinkActive] = useState("");
+
+    const LinkhandleClick = (to) => {
+        setLinkActive(to);
+    }
+
     return (
         <header id="header" role='banner'>
             <div className="header__left">
@@ -17,10 +23,10 @@ const Header = () => {
 
                 <nav className="nav">
                     <ul>
-                        <li><Link to="/map" className='active'>ADD MAP</Link></li>
-                        <li><Link to="/comm">ADD COMM</Link></li>
-                        <li><Link to="/score">ADD SCORE</Link></li>
-                        <li><Link to="/">MYPAGE</Link></li>
+                        <li><Link to="/map" className={linkActive === "/map" ? 'active' : ""} onClick={() => LinkhandleClick("/map")}>ADD MAP</Link></li>
+                        <li><Link to="/comm" className={linkActive === "/comm" ? 'active' : ""} onClick={() => LinkhandleClick("/comm")}>ADD COMM</Link></li>
+                        <li><Link to="/score" className={linkActive === "/score" ? 'active' : ""} onClick={() => LinkhandleClick("/score")}>ADD SCORE</Link></li>
+                        <li><Link to="/mypage" className={linkActive === "/mypage" ? 'active' : ""} onClick={() => LinkhandleClick("/mypage")}>MYPAGE</Link></li>
                     </ul>
                 </nav>
             </div>
